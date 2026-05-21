114d1fa23d2ec6dcbb3e51f39cb099c0174e99e1
Yuv-12 <yuvrajgupta1209@gmail.com>
2026-05-21 18:32:21 +0530

    implement O(N+M) approach to find closest pair from two arrays

---
+++ Searching/findTheClosestPairFromTwoArrays.cpp
@@ -0,0 +1,41 @@
+/*Given two sorted arrays arr1[] and arr2[] of size n and m and a number x,
+find the pair whose sum is closest to x and the pair has an element from each array.
+In the case of multiple closest pairs return any one of them.
+*/
+// Solution
+/*Start:
+    from the beginning of arr1
+    and from the end of arr2
+    Compute pair sum.
+    If sum is smaller than x, move i forward to increase sum.
+    If sum is larger than x, move j backward to decrease sum.
+    Keep track of the pair having minimum difference from x.*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> findClosestPair(vector<int> &arr1, vector<int> &arr2, int x)
+{
+    int i = 0;
+    int j = arr2.size() - 1;
+    int minDiff = INT_MAX;
+    int ans1 = 0;
+    int ans2 = 0;
+    while (i <= arr1.size() - 1 && j >= 0)
+    {
+        int sum = arr1[i] + arr2[j];
+        if (abs(sum - x) < minDiff)
+        {
+            minDiff = abs(sum - x);
+            ans1 = arr1[i];
+            ans2 = arr2[j];
+        }
+        if (sum == x)
+            return {ans1, ans2};
+        else if (sum < x)
+            i++;
+        else
+            j--;
+    }
+    return { ans1, ans2 };
+}
