ec4336af5b22800742a4e8cc51e264c005cdd51b
Yuvraj Gupta <yuvrajgupta1209@gmail.com>
2026-08-02 01:07:50 +0530

    Add solution - Inorder Traversal (CPP)

--- Easy/Inorder Traversal/Solution.cpp
+++ Easy/Inorder Traversal/Solution.cpp
@@ -13,16 +13,37 @@ class Node {
 
 class Solution {
   public:
-  vector<int> res;
+  
     vector<int> inOrder(Node* root) {
         // code here
-        
-        if(root == NULL )
-            return{};
-        
-        inOrder(root->left);
-        res.push_back(root->data);
-        inOrder(root->right);
+        vector<int> res;
+        Node *curr = root;
+        while(curr!=NULL)
+        {
+            if(curr->left==NULL)
+            {
+                res.push_back(curr->data);
+                curr = curr->right;
+            }
+            else
+            {
+                Node *prev = curr->left;
+                while(prev->right != NULL && prev->right!=curr )
+                    prev = prev->right;
+            
+                if(prev->right == NULL)
+                {
+                    prev->right = curr;
+                    curr = curr->left;
+                }
+                else
+                {
+                    prev->right = NULL;
+                    res.push_back(curr->data);
+                    curr = curr->right;
+                }
+            }
+        }           
         return res;
     }
 };
