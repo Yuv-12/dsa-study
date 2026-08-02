35002869bce5b6a21496bb625f71b17465d39f04
Yuvraj Gupta <yuvrajgupta1209@gmail.com>
2026-08-02 12:18:28 +0530

    Add solution - Lowest Common Ancestor In A Binary Tree (CPP)

---
+++ Medium/Lowest Common Ancestor In A Binary Tree/Solution.cpp
@@ -0,0 +1,29 @@
+/*Structure of tree Node
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};*/
+
+class Solution {
+  public:
+    Node* lca(Node* root, int n1, int n2) {
+        //  code here
+        if(root==NULL) return NULL;
+        if(root->data ==n1 || root->data ==n2)  return root;
+        Node * lca1 = lca(root->left,n1,n2);
+        Node *lca2 = lca(root->right,n1,n2);
+        if(lca1!=NULL && lca2!=NULL)
+            return root;
+        if(lca1!=NULL)
+            return lca1;
+        else
+            return lca2;
+    }
+};
