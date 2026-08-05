5caf66f0d81854b5f5959438148bcd6086aa2181
Yuvraj Gupta <yuvrajgupta1209@gmail.com>
2026-08-06 00:07:39 +0530

    Add solution - Print Common Nodes In Bst (CPP)

--- Easy/Print Common Nodes In Bst/Solution.cpp
+++ Easy/Print Common Nodes In Bst/Solution.cpp
@@ -13,24 +13,48 @@ class Node {
 
 class Solution {
   public:
-  void inorder(Node *r,map<int,int>&freq)
-  {
-      if(r == NULL)
-        return;
-        inorder(r->left,freq);
-        freq[r->data]++;
-        inorder(r->right,freq);
-  }
     vector<int> findCommon(Node* r1, Node* r2) {
         // code here
-       map<int,int> freq;
+       stack<Node *> s1,s2;
         vector<int> res;
-        inorder(r1,freq);
-        inorder(r2,freq);
-        for(auto it : freq)
+        while(true)
         {
-            if(it.second == 2)
-                res.push_back(it.first);
+            while(r1)
+            {
+                s1.push(r1);
+                r1 = r1->left;
+            }
+            while(r2)
+            {
+                s2.push(r2);
+                r2 = r2->left;
+            }
+            if(s1.empty() || s2.empty())
+                break;
+            Node *curr1 = s1.top();
+            Node *curr2 = s2.top();
+            
+            if(curr1->data == curr2->data)
+            {
+                res.push_back(curr1->data);
+                s1.pop();
+                s2.pop();
+                
+                r1 = curr1->right;
+                r2 = curr2->right;
+            }
+            else if(curr1->data < curr2->data)
+            {
+                s1.pop();
+                r1 = curr1->right;
+                r2  = NULL;
+            }
+            else
+            {
+                s2.pop();
+                r2 = curr2->right;
+                r1 = NULL;
+            }
         }
         return res;
     }
