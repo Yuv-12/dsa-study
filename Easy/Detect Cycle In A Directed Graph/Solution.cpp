e978887abe0f8ed3548294245d8b86deae82862f
Yuvraj Gupta <yuvrajgupta1209@gmail.com>
2026-08-10 16:41:09 +0530

    Add solution - Detect Cycle In A Directed Graph (CPP)

--- Easy/Detect Cycle In A Directed Graph/Solution.cpp
+++ Easy/Detect Cycle In A Directed Graph/Solution.cpp
@@ -1,42 +1,42 @@
 class Solution {
   public:
-  void addEdge(vector<vector<int>> &adj,int u,int v)
+  void addEdge(vector<vector<int>> &adj,int u,int v,vector<int> &indegree)
   {
       adj[u].push_back(v);
+      indegree[v]++;
   }
   
-  bool DFS(vector<vector<int>>&adj,int src,vector<bool>&visited,vector<bool>& reccSt)
-  {
-      visited[src] = true;
-      reccSt[src] = true;
-      for(int u : adj[src])
-      {
-          if(visited[u]==false && DFS(adj,u,visited,reccSt))
-            return true;
-        else if(reccSt[u])
-            return true;
-      }
-      reccSt[src] = false;
-      return false;
-  }
+ 
     bool isCyclic(int V, vector<vector<int>> &edges) {
         // code here
         vector<vector<int>> adj(V);
+        vector<int> indegree(V,0);
         for(int i = 0;i<edges.size();i++)
         {
-            addEdge(adj,edges[i][0],edges[i][1]);
+            addEdge(adj,edges[i][0],edges[i][1],indegree);
         }
-        
-        vector<bool> visited(V,false);
-        vector<bool> reccSt(V,false);
+        queue<int> q;
         for(int i = 0;i<V;i++)
         {
-            if(visited[i]==false)
-            {
-                if(DFS(adj,i,visited,reccSt))
-                    return true;
-            }
+            if(indegree[i]==0)
+                q.push(i);
         }
-        return false;
+        vector<int> res;
+        int count = 0;
+        while(!q.empty())
+        {
+            int u = q.front();
+            q.pop();
+            res.push_back(u);
+            for(int v : adj[u])
+            {
+                indegree[v]--;
+                if(indegree[v]==0)
+                    q.push(v);
+            }
+            count++;
+            
+        }
+        return (count!=V);
     }
 };
