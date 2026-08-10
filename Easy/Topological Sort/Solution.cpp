1b9d64e928e34d29064767a21b5dc43004fa11ed
Yuvraj Gupta <yuvrajgupta1209@gmail.com>
2026-08-10 16:04:36 +0530

    Add solution - Topological Sort (CPP)

---
+++ Easy/Topological Sort/Solution.cpp
@@ -0,0 +1,39 @@
+class Solution {
+  public:
+    void addEdge(vector<vector<int>>&adj,int u,int v,vector<int>&indegree)
+    {
+        adj[u].push_back(v);
+        indegree[v]++;
+    }
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<int> res;
+        vector<vector<int>> adj(V);
+        vector<int>  indegree(V,0);
+        for(int i = 0;i<edges.size();i++)
+        {
+            addEdge(adj,edges[i][0],edges[i][1],indegree);
+        }
+        queue<int> q;
+        for(int i = 0;i<V;i++)
+        {
+            if(indegree[i]==0)
+                q.push(i);
+                
+        }
+        while(!q.empty())
+        {
+            int u = q.front();
+            q.pop();
+            res.push_back(u);
+            for( int v : adj[u])
+            {
+                indegree[v]--;
+                if(indegree[v]==0)
+                    q.push(v);
+            }
+            
+        }
+        return res;
+    }
+};
