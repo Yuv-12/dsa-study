b3a32e6d1b6f5cafea5c19c044ff1d90362f43ce
Yuvraj Gupta <yuvrajgupta1209@gmail.com>
2026-08-20 23:20:15 +0530

    Add solution - Josephus Problem (CPP)

---
+++ Hard/Josephus Problem/Solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+  int jos(int n, int k)
+  {
+      if(n==1)
+        return 0;
+        else
+            return (jos(n-1,k)+k)%n;
+  }
+    int josephus(int n, int k) {
+        // code here
+        return jos(n,k)+1;
+    }
+};
