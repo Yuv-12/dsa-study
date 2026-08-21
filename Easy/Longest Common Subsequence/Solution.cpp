c98a982de44a745d9385214f1cf65d1319c93ef0
Yuvraj Gupta <yuvrajgupta1209@gmail.com>
2026-08-21 16:07:37 +0530

    Add solution - Longest Common Subsequence (CPP)

---
+++ Easy/Longest Common Subsequence/Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+  int solve (string &s1, string &s2,int m,int n,vector<vector<int>>&dp)
+  {
+      if(m==0 || n==0)
+        return 0;
+    
+    if(s1[m-1] == s2[n-1])
+        return 1+solve(s1,s2,m-1,n-1,dp);
+    if(dp[m][n]!=-1)
+        return dp[m][n];
+    return dp[m][n] = max(solve(s1,s2,m,n-1,dp),solve(s1,s2,m-1,n,dp));
+  }
+    int lcs(string &s1, string &s2) {
+        // code here
+        int m = s1.size();
+        int n = s2.size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        return solve(s1,s2,m,n,dp);
+    }
+};
