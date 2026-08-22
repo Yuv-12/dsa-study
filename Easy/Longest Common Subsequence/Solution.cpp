946b33fa6924072b0534e7bacced5888a5cbebe2
Yuvraj Gupta <yuvrajgupta1209@gmail.com>
2026-08-22 23:01:24 +0530

    Add solution - Longest Common Subsequence (CPP)

--- Easy/Longest Common Subsequence/Solution.cpp
+++ Easy/Longest Common Subsequence/Solution.cpp
@@ -1,21 +1,29 @@
 class Solution {
   public:
-  int solve (string &s1, string &s2,int m,int n,vector<vector<int>>&dp)
-  {
-      if(m==0 || n==0)
-        return 0;
-    
-    if(s1[m-1] == s2[n-1])
-        return dp[m][n] = 1+solve(s1,s2,m-1,n-1,dp);
-    if(dp[m][n]!=-1)
-        return dp[m][n];
-    return dp[m][n] = max(solve(s1,s2,m,n-1,dp),solve(s1,s2,m-1,n,dp));
-  }
     int lcs(string &s1, string &s2) {
         // code here
         int m = s1.size();
         int n = s2.size();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        return solve(s1,s2,m,n,dp);
+        int dp[m+1][n+1];
+        for(int i = 0;i<=m;i++)
+        {
+            dp[i][0] = 0;
+        }
+        for(int j = 0;j<=n;j++)
+        {
+            dp[0][j] = 0;
+        }
+        
+        for(int i = 1;i<=m;i++)
+        {
+            for(int j = 1;j<=n;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[m][n];
     }
 };
